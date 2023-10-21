d8c7345135d179a07fc4240057ebb593dfaec0cf
RAGHAVENDRA REDDY DEVIREDDY <drvrdevireddy@gmail.com>
2023-10-21 15:18:01 +0000

    fragment width,height changes

---
+++ app/cricketui/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","wc/india/cricketui/model/models"],function(e,i,t){"use strict";return e.extend("wc.india.cricketui.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(t.createDeviceModel(),"device")}})});
